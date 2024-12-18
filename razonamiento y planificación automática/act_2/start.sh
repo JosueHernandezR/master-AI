0c3be1cce42219194bcaa4a63553ea16cb73fc3a
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2024-12-18 10:31:53 -0600

    Improve pddl ✈️

--- razonamiento y planificación automática/act_2/start.sh
+++ razonamiento y planificación automática/act_2/start.sh
@@ -6,10 +6,10 @@ mkdir rundir
 cp /usr/local/actividad3/domain.pddl rundir
 cp /usr/local/actividad3/problem1.pddl rundir
 RUNDIR="$(pwd)/rundir"
-DOMAIN="$RUNDIR/domain.pddl"
-PROBLEM="$RUNDIR/problem1.pddl"
+DOMAIN="$RUNDIR/domain_minerales.pddl"
+PROBLEM="$RUNDIR/problem_minerales_v2.pddl"
 PLANFILE="$RUNDIR/sas_plan"
 COSTBOUND=42 
 ulimit -t 1800
 ulimit -v 8388608
-singularity run -C -H $RUNDIR planner.img $DOMAIN $PROBLEM $PLANFILE $COSTBOUND
+sudo singularity run -C -H $RUNDIR planner.img $DOMAIN $PROBLEM $PLANFILE $COSTBOUND
